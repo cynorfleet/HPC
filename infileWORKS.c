6959136a8bf5ef1a5097b32fdcad27dee3c96261
Christian Norfleet <cynorfleet@gmail.com>
2017-03-25 20:45:26 -0500

    need ti read in start node

--- infileWORKS.c
+++ infileWORKS.c
@@ -3,40 +3,57 @@
 #include<stdio.h>
 #include<string.h>
 #include<math.h>
+
 #define IN 99
 #define N 6
 
 int dijkstra(int cost[][N], int source, int target);
-int main()
+void strrev(char str[100]);
+void openFiles(FILE *infile, FILE *outfile);
+
+int main(int argc, char *argv[])
 {
-    char node[100];
-    FILE *infile;
-    infile=fopen("infile.txt", "r");
+    int node_count = atoi(argv[1]) - 1;
+    int edge_count = node_count*(node_count-1)/2;
+    printf("node count: %d edge count: %d\n", node_count, edge_count);
+
+    char tempnode[100];
+    int node, out_counter=1;
+    FILE *infile, *outfile;
+    openFiles(infile,outfile);
 
     int cost[N][N],i,j,w,ch,co;
     int source, target,x,y;
     printf("\t The Shortest Path Algorithm ( DIJKSTRA'S ALGORITHM in C \n\n");
     for(i=1;i< N;i++)
     for(j=1;j< N;j++)
-    cost[i][j] = IN;
+    cost[i][j] = edge_count;
     for(x=1;x< N;x++)
     {
         for(y=x+1;y< N;y++)
         {
-            fscanf(infile, "%[^\n]\n", node);
-            printf("Weight of the path between nodes %d and %d: %d\n",x,y,atoi(node));
-            cost [x][y] = cost[y][x] = w;
+            fscanf(infile, "%[^\n]\n", tempnode);
+            node = atoi(tempnode);
+            printf("Weight of the path between nodes %d and %d: %d\n",x,y,node);
+            cost [x][y] = cost[y][x] = node;
         }
         printf("\n");
     }
-    printf("\nEnter the source:");
+    printf("\nEnter the source: ");
     scanf("%d", &source);
-    printf("\nEnter the target");
-    scanf("%d", &target);
-    co = dijsktra(cost,source,target);
-    printf("\nThe Shortest Path: %d",co);
+
+    for( out_counter= 1; out_counter <= node_count; out_counter++)
+    {
+        if(out_counter != source)
+        {
+            co = dijsktra(cost,source,out_counter);
+            printf("\tThe Shortest Path from %d to %d: %d\n",source,out_counter,co);
+        }
+    }
     fclose(infile);
+    fclose(outfile);
 }
+
 int dijsktra(int cost[][N],int source,int target)
 {
     int dist[N],prev[N],selected[N]={0},i,m,min,start,d,j;
@@ -78,6 +95,30 @@ int dijsktra(int cost[][N],int source,int target)
         start = prev[start];
     }
     path[j]='\0';
+    strrev(path);
     printf("%s", path);
     return dist[target];
+}
+
+void openFiles(FILE *infile, FILE *outfile)
+{
+  infile=fopen("infile.txt", "r");
+  outfile=fopen("outfile.txt", "w");
+}
+void strrev(char str[100])
+{
+    char temp;
+    int i, j = 0;
+
+    i = 0;
+    j = strlen(str) - 1;
+
+    while (i < j)
+    {
+       temp = str[i];
+       str[i] = str[j];
+       str[j] = temp;
+       i++;
+       j--;
+    }
 }
